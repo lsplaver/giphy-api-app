218036a7d2d957be6ee9ece5cb375a46b6c1e74b
unknown <lsplaver@yahoo.com>
2018-03-10 15:30:13 -0500

    Update to clean the code of any comments

--- assets/javascript/homework-giphy-api.js
+++ assets/javascript/homework-giphy-api.js
@@ -1,49 +1,34 @@
 $(document).ready(function() {
-    // declaring global variables
     var topics = [];
 
-    // function to create the buttons container section of the page
     function initializeButtonsContainer() {
-        // for loop to create the buttons for each index of the topics array
         for (var x = 0; x < topics.length; x++) {
             $("#buttonsContainer").append($("<button>").attr("id", topics[x]).attr("class", "topics-info").text(topics[x]));
         }
     }
 
-    // function to create the add a topic section of the page
     function initializeAddTopicContainer() {
-        // creates a variable for creating new elements
         var newElement, targetDiv = $("#addTopicContainer");
 
-        // creates and adds the text for a new label element
         newElement = $("<label>").text("Add a topic");
 
-        // appends the label element to the add topic container
         $(targetDiv).append(newElement);
 
-        // appends the breakline element to the add topic container
         $(targetDiv).append($("<br><br>"));
         
-        // creates and adds the id and type attributes for a new textbox input element
         newElement = $("<input>").attr("id", "topicInput").attr("type", "text");
 
-        // appends the input element to the add topic container
         $(targetDiv).append(newElement);
 
-        // appends another breakline element to the add topic container
         $(targetDiv).append($("<br><br>"));
 
-        // creates and adds the attributes for a new submit button element
         newElement = $("<button>").attr("id", "btnSubmit").attr("type", "submit").text("Submit");
 
-        // appends the new submit button element to the add topic container
         $(targetDiv).append(newElement);
     }
 
-    // current test values for initializing topics array
     topics = ["dog", "cat", "rabbit", "aragorn", "arnold schwarzenegger"];
 
-    // call initialize functions
     initializeButtonsContainer(),initializeAddTopicContainer();
 
     function addNewTopicButton(newTopic) {
@@ -81,29 +66,24 @@ $(document).ready(function() {
                     results[x].images.fixed_height.url).attr("data-state", "still").attr("class", "gifs");
 
                 $(resultsDiv).append(ratingLabel);
-                $(/*topicImage*/resultsDiv).append($("<br><br>"));
-                $(/*ratingLabel*/ resultsDiv).append(topicImage);
+                $(resultsDiv).append($("<br><br>"));
+                $(resultsDiv).append(topicImage);
                 $("#gifResultsContainer").append(resultsDiv);
             }
         });
     }
 
-    // event listener for when a button is clicked
     $("#btnSubmit").on("click", function(event) {
-        // prevents the form from immediately submitting
         event.preventDefault();
 
-        // creates a variable to temporarily hold the id of the button clicked
         var btnTargetId = $(this).attr("id");
 
-        // check to see which type of button was clicked - btnSubmit or one of the btnTopic buttons
             var newTopic = $("#topicInput").val().trim();
             addNewTopicButton(newTopic);
     });
 
     $(document).on("click", ".topics-info", displayResults);
 
-    // $(".gifs").on("click", function() {
     $(document).on("click", ".gifs", function() {
         var currentState = $(this).attr("data-state");
         var animateURL = $(this).attr("data-animate");
